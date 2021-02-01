b5b174f1cd8bcd64486269c6b56af831f9605dde
ekincanufuktepe <ekincanufuktepe@gmail.com>
2021-02-01 12:48:06 -0600

    Week 1 codes added

---
+++ week1/hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include "hello_functions.h"
+
+int main() {
+	print_message();
+	return 0;
+}
---
+++ week1/hello_functions.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include "hello_functions.h"
+
+void print_message() {
+	printf("Not another hello world message...\n");
+}
+

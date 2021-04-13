80dccab102357b59d23d6490de100ba27df2694a
ekincanufuktepe <ekincanufuktepe@gmail.com>
2021-04-13 13:08:08 -0500

    Comparator example added in week12 codes
    JediComparator added, in addition with an example of sending an outside function address to list.sort() built-in function.

--- week12/ex3_template.cpp
+++ week12/ex3_template.cpp
@@ -30,8 +30,15 @@ struct Jedi {
  
 struct JediComparator {
 	// Compare 2 Jedi objects using name
-	
+	bool operator ()(const Jedi &jedi1, const Jedi &jedi2){
+		return jedi1.name <= jedi2.name;
+	}
 };
+
+bool jedi_compare(const Jedi &jedi1, const Jedi &jedi2){
+		return jedi1.name <= jedi2.name;
+}
+
 int main() {
 	list<Jedi> listOfJedis = { Jedi(2200, "Luke Skywalker"),
 				Jedi(1750, "Obi-wan Kenobi"),
@@ -63,9 +70,23 @@ int main() {
 				});
 	for(Jedi &jedi : listOfJedis)
 		cout << jedi.midichlorians << " :: " << jedi.name << endl;
- 
+	
+	cout << "==============SORTING WITH COMPARATOR==============" << endl;
 	// Sorting List using Function Objects as comparator
+	listOfJedis.sort(JediComparator());
 
+	for(Jedi &jedi : listOfJedis)
+		cout << jedi.midichlorians << " :: " << jedi.name << endl;
  
 	return EXIT_SUCCESS;
 }
+
+
+
+
+
+
+
+
+
+
